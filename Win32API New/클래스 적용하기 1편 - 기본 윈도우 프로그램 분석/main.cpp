2c375b251239e6d56bd2b64bd3fad9e54bf0a922
ruvendix <ruvendix@naver.com>
2019-04-07 21:24:37 +0900

    기본 윈도우 프로그램 분석

---
+++ Win32API New/클래스 적용하기 1편 - 기본 윈도우 프로그램 분석/main.cpp
@@ -0,0 +1,51 @@
+#include <windows.h>
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT32 message, WPARAM wParam, LPARAM lParam);
+
+INT32 APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, INT32 cmdShow)
+{
+	WNDCLASSEX wcex    = { sizeof(wcex) };
+	wcex.hInstance     = hInstance;
+	wcex.lpszClassName = L"Base Window Program";
+	wcex.lpfnWndProc   = WindowProcedure;
+	wcex.hbrBackground = static_cast<HBRUSH>(::GetStockObject(GRAY_BRUSH));
+
+	::RegisterClassEx(&wcex);
+
+	HWND hWnd = ::CreateWindow(wcex.lpszClassName, wcex.lpszClassName, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 1024, 768, nullptr, nullptr, hInstance, nullptr);
+
+	UpdateWindow(hWnd); 
+	ShowWindow(hWnd, cmdShow);
+	
+	MSG message;
+	::ZeroMemory(&message, sizeof(message));
+
+	while (true)
+	{
+		if (message.message == WM_QUIT)
+		{
+			break;
+		}
+
+		if (::GetMessage(&message, nullptr, 0, 0))
+		{
+			::TranslateMessage(&message);
+			::DispatchMessage(&message);
+		}
+	}
+
+	return static_cast<INT32>(message.wParam);
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT32 message, WPARAM wParam, LPARAM lParam)
+{
+	if (message == WM_DESTROY)
+	{
+		::PostQuitMessage(EXIT_SUCCESS);
+		return S_OK;
+	}
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
