589eaa0089435ad85b691d823caeb738e3161a43
ruvendix <ruvendix@naver.com>
2019-05-11 17:06:42 +0900

    마우스 드래그로 사각형 그리는 부분에 주석 추가

--- Win32API New/007. 마우스 드래그로 사각형 그리기/MouseDragWnd.cpp
+++ Win32API New/007. 마우스 드래그로 사각형 그리기/MouseDragWnd.cpp
@@ -1,5 +1,6 @@
 #include "MouseDragWnd.h"
 
+// WM_LBUTTONDOWN 핸들러입니다.
 void RXMouseDragWnd::OnLButtonDown(HWND hWnd, BOOL bDoubleClick, INT32 x, INT32 y, UINT keyFlags)
 {
 	m_bDrag = true;
@@ -7,6 +8,7 @@ void RXMouseDragWnd::OnLButtonDown(HWND hWnd, BOOL bDoubleClick, INT32 x, INT32 
 	m_dragArea.top  = y;
 }
 
+// WM_LBUTTONUP 핸들러입니다.
 void RXMouseDragWnd::OnLButtonUp(HWND hWnd, INT32 x, INT32 y, UINT keyFlags)
 {
 	m_bDrag = false;
@@ -14,6 +16,7 @@ void RXMouseDragWnd::OnLButtonUp(HWND hWnd, INT32 x, INT32 y, UINT keyFlags)
 	::InvalidateRect(hWnd, nullptr, TRUE);
 }
 
+// WM_MOUSEMOVE 핸들러입니다.
 void RXMouseDragWnd::OnMouseMove(HWND hWnd, INT32 x, INT32 y, UINT keyFlags)
 {
 	if (m_bDrag == true)
@@ -24,11 +27,16 @@ void RXMouseDragWnd::OnMouseMove(HWND hWnd, INT32 x, INT32 y, UINT keyFlags)
 	}
 }
 
+// WM_PAINT 핸들러입니다.
 void RXMouseDragWnd::OnPaint(HWND hWnd)
 {
+	// 반드시 DC를 BeginPaint()로 생성해야 합니다.
+	// 그렇지 않으면 WM_PAINT는 삭제되지 않습니다.
 	PAINTSTRUCT ps;
 	HDC hDC = BeginPaint(hWnd, &ps);
 
+	// ======================================================================
+	// 논리 펜을 생성해서 DC에 적용합니다.
 	LOGPEN logPen;
 	logPen.lopnColor   = RGB(255, 255, 0);
 	logPen.lopnStyle   = PS_SOLID;
@@ -38,11 +46,16 @@ void RXMouseDragWnd::OnPaint(HWND hWnd)
 	HPEN   hOldPen   = SelectPen(hDC, hPen);
 	HBRUSH hOldBrush = SelectBrush(hDC, GetStockBrush(NULL_BRUSH));
 
+	// ======================================================================
+	// 드래그 영역을 사각형으로 그립니다.
 	::Rectangle(hDC, m_dragArea.left, m_dragArea.top, m_dragArea.right, m_dragArea.bottom);
 
+	// ======================================================================
+	// 이전 펜으로 복구하고 생성한 펜을 제거합니다.
 	SelectBrush(hDC, hOldBrush);
 	SelectPen(hDC, hOldPen);
 	DeletePen(hPen);
 
+	// BeginPaint()로 시작했으면 EndPaint()로 끝내야 합니다.
 	EndPaint(hWnd, &ps);
 }
